8ad572ad2e6a1d58da5983e827a538dec54198d4
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2025-12-17 14:53:44 -0500

    chore(structure): move constants to shared folder

---
+++ src/shared/constants/Colors.ts
@@ -0,0 +1,18 @@
+export default {
+  light: {
+    text: "#000",
+    primaryColor: '#A1B2A8 ',
+    secondaryColor: '#B2C8B4',
+    tertiaryColor: '#C4D6C1',
+    quaternaryColor: '#E0E4D7',
+    quinaryColor: '#F0F4E1',
+  },
+  dark: {
+    text: '#fff',
+    primaryColor: '#fff',
+    secondaryColor: '#193150',
+    tertiaryColor: '#132740',
+    quaternaryColor: '#0e1e31',
+    quinaryColor: '#081421',
+  },
+};
